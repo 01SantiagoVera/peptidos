b56e09059162e0121755acb36d939a32eaccb1d5
Santiago <00verasantiago@gmail.com>
2024-11-09 20:31:37 -0500

    ajuste prediccion

--- app/models/predictionModels.py
+++ app/models/predictionModels.py
@@ -1,13 +1,17 @@
 import os
 import pickle
 
-# Mapa de codificación básico de aminoácidos a números
+
+
+
 amino_acid_map = {
     'A': 1, 'R': 2, 'N': 3, 'D': 4, 'C': 5,
     'Q': 6, 'E': 7, 'G': 8, 'H': 9, 'I': 10,
     'L': 11, 'K': 12, 'M': 13, 'F': 14, 'P': 15,
     'S': 16, 'T': 17, 'W': 18, 'Y': 19, 'V': 20
 }
+
+
 
 
 def encode_sequence(sequence, fixed_length=1477):
@@ -20,12 +24,13 @@ def encode_sequence(sequence, fixed_length=1477):
     else:
         encoded_sequence = encoded_sequence[:fixed_length]  # Truncar si es más larga
 
+
     return encoded_sequence
 
 
 def load_models():
     # Ruta base para cargar los modelos
-    base_path = os.path.dirname(__file__)  # Obtiene el directorio actual de este archivo
+    base_path = os.path.dirname(__file__)
 
     # Rutas absolutas a los archivos .pkl
     model_paths = {
@@ -49,7 +54,6 @@ def load_models():
     return model_SVM, model_RF, model_NN
 
 
-
 def make_prediction(models, sequence):
     model_SVM, model_RF, model_NN = models
 
@@ -61,8 +65,24 @@ def make_prediction(models, sequence):
     prediction_rf = model_RF.predict([encoded_sequence])
     prediction_nn = model_NN.predict([encoded_sequence])
 
+    print("SVM Prediction:", prediction_svm)
+    print("RF Prediction:", prediction_rf)
+    print("NN Prediction:", prediction_nn)
+
+
+    # Calcular cuántos modelos predicen 1.0 (es decir, es un péptido)
+    positive_predictions = sum([prediction_svm[0] == 1.0, prediction_rf[0] == 1.0, prediction_nn[0] == 1.0])
+
+    # Si al menos dos modelos predicen 1.0, se considera un péptido
+    if positive_predictions >= 2:
+        final_prediction = "Es un péptido"
+    else:
+        final_prediction = "No es un péptido"
+
+    # Retornar las predicciones individuales y la predicción final
     return {
         "SVM": prediction_svm[0],
         "RandomForest": prediction_rf[0],
-        "NeuralNetwork": prediction_nn[0]
+        "NeuralNetwork": prediction_nn[0],
+        "final_prediction": final_prediction
     }
